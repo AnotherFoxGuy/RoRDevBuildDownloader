7180cf4c32e3faebd34b0143cc0f8f05219c73a0
Edgar <edgar@anotherfoxguy.com>
2018-04-23 09:05:56 +0200

    Added project files.

---
+++ RoRDevBuildDownloader/MainWindow.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace RoRDevBuildDownloader
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.LogWindow = new System.Windows.Forms.TextBox();
+			this.progressBar = new System.Windows.Forms.ProgressBar();
+			this.UpdateButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// LogWindow
+			// 
+			this.LogWindow.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.LogWindow.Location = new System.Drawing.Point(12, 12);
+			this.LogWindow.Multiline = true;
+			this.LogWindow.Name = "LogWindow";
+			this.LogWindow.ReadOnly = true;
+			this.LogWindow.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.LogWindow.Size = new System.Drawing.Size(460, 501);
+			this.LogWindow.TabIndex = 0;
+			// 
+			// progressBar
+			// 
+			this.progressBar.Location = new System.Drawing.Point(12, 519);
+			this.progressBar.Name = "progressBar";
+			this.progressBar.Size = new System.Drawing.Size(365, 30);
+			this.progressBar.TabIndex = 1;
+			// 
+			// UpdateButton
+			// 
+			this.UpdateButton.Enabled = false;
+			this.UpdateButton.Location = new System.Drawing.Point(383, 519);
+			this.UpdateButton.Name = "UpdateButton";
+			this.UpdateButton.Size = new System.Drawing.Size(90, 30);
+			this.UpdateButton.TabIndex = 3;
+			this.UpdateButton.Text = "Update";
+			this.UpdateButton.UseVisualStyleBackColor = true;
+			this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(484, 561);
+			this.Controls.Add(this.UpdateButton);
+			this.Controls.Add(this.progressBar);
+			this.Controls.Add(this.LogWindow);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "RoR dev build downloader";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox LogWindow;
+		private System.Windows.Forms.ProgressBar progressBar;
+		private System.Windows.Forms.Button UpdateButton;
+	}
+}
+
---
+++ RoRDevBuildDownloader/MainWindow.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RoRDevBuildDownloader
+{
+	public partial class MainForm : Form
+	{
+		WebClient client;
+
+		string latestVersion = null;
+		string installedVersion;
+		string dlURL;
+		string fileName;
+
+		public MainForm()
+		{
+			InitializeComponent();
+
+			installedVersion = Properties.Settings.Default.InstalledVersion;
+			fileName = $"{Path.GetTempPath()}RoRDevBuild.zip";
+
+			LOG("Getting newest version...");
+			XmlDocument doc = new XmlDocument();
+			doc.Load("https://sourceforge.net/p/rigs-of-rods/activity/feed.rss");
+
+			//Display all the book titles.
+			var elemList = doc.GetElementsByTagName("item");
+			var pat = @"RoR.*CIBuild-\d*\.zip";
+			var reg = new Regex(pat, RegexOptions.IgnoreCase);
+			for (int i = 0; i < elemList.Count; i++)
+			{
+				var x = elemList[i]["title"];
+				//LOG(x.InnerText); 
+				Match m = reg.Match(x.InnerText);
+				if(m.Success)
+				{					
+					LOG($"Version found: {m.Value}");
+					dlURL = elemList[i]["link"].InnerText;
+					latestVersion = m.Value;
+					break;
+				}
+			}
+
+			if(latestVersion != null)
+			{
+				UpdateButton.Enabled = true;
+				if (latestVersion == installedVersion)
+					MessageBox.Show($"Already up to date");
+			}
+		}
+
+		private void UpdateButton_Click(object sender, EventArgs e)
+		{
+			LOG($"Downloading: {dlURL}");
+
+			client = new WebClient();
+			client.DownloadFileAsync(new Uri(dlURL), fileName);
+			client.DownloadProgressChanged += UpdateDlProgress;
+			client.DownloadFileCompleted += ExtractZip;
+		}
+
+		private void UpdateDlProgress(object sender, DownloadProgressChangedEventArgs e)
+		{
+			progressBar.Value = e.ProgressPercentage;
+		}
+
+		private void ExtractZip(object sender, AsyncCompletedEventArgs e)
+		{
+			LOG($"Extracting zip: {fileName} to {Application.StartupPath}");
+			ZipFile.ExtractToDirectory(fileName, Application.StartupPath);
+			Properties.Settings.Default.InstalledVersion = latestVersion;
+			LOG("Done!");
+			MessageBox.Show($"Done!\nUpdated from {installedVersion} to {latestVersion}");
+		}
+
+		private void LOG(string txt)
+		{
+			LogWindow.AppendText($"{txt}\n");
+		}
+	}
+}
